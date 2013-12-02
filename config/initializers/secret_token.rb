f18d1f5e5254ee52da3817ad8a3850fe31e43937
shyam <shyam.salim.kumar@outlook.com>
2013-12-02 16:11:33 +0530

    - Nothing gr8. Just adding the first files for the RoR study.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'd9bf5d7a6f3c33254bf13f0d3afd2ddf899b0cef47ffc4ca48418e1bfef1277bea49a091e3413b85c3e723c020e0364ef23686737853c000f58d66259dbeccb8'
